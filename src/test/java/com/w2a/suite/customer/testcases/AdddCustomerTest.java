c71d5fa43d3a7432c583b2ae9ed6066243803205
Neeraj <neerajakshulu.allala@gmail.com>
2018-03-04 22:16:39 +0530

    Commiting the changes

--- src/test/java/com/w2a/suite/customer/testcases/AdddCustomerTest.java
+++ src/test/java/com/w2a/suite/customer/testcases/AdddCustomerTest.java
@@ -11,6 +11,8 @@ import com.w2a.utilities.DataUtil;
 import com.w2a.utilities.ExcelReader;
 
 public class AdddCustomerTest {
+	
+	//This is the modified
 	@Test(dataProviderClass=DataProviders.class,dataProvider="bankManagerDP")
 	public void addCustomerTest(Hashtable<String,String> data){
 		
